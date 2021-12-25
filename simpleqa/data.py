e27709a2af5f57d486d842796f3bf2946ae4d6af
Huu Hiep Nguyen <hiepnh.vtt@gmail.com>
2021-12-25 16:23:34 +0700

    Add files via upload

--- simpleqa/data.py
+++ simpleqa/data.py
@@ -22,10 +22,10 @@ def get_input(text, target, loc, max_len = 384, doc_stride = 128):
             ends.append(loc - i + len(target))
     return input_ids, masks, starts, ends
 
-def read_data(file, target, tokenizer, max_len = 384, doc_stride = 128):
+def read_data(file, target_name, tokenizer, max_len = 384, doc_stride = 128):
     data = json.load(open(file, encoding='utf-8'))
     texts = [item['text'] for item in data]
-    targets = [item[target] for item in data]
+    targets = [item[target_name] for item in data]
 
     text_tok = [tokenizer.encode(text, add_special_tokens =False) for text in texts]
     target_tok = [tokenizer.encode(text, add_special_tokens =False) for text in targets]
@@ -45,3 +45,39 @@ def read_data(file, target, tokenizer, max_len = 384, doc_stride = 128):
         sample_loc.append(sample_loc[-1] + len(input_ids))
     
     return all_input_ids, all_masks, all_starts, all_ends, sample_loc
+
+def get_input_eval(text, max_len = 384, doc_stride = 128):
+    input_ids = []
+    masks = []
+    starts = []
+    ends = []
+    for i in range(0, len(text), doc_stride):
+        ntok = min(i+max_len, len(text)) - i
+        ids = text[i:min(i+max_len, len(text))] + [0]*(max_len-ntok)
+        input_ids.append(ids)
+        masks.append([1]*ntok + [0]*(max_len-ntok))
+        starts.append(i) # dummy
+        ends.append(i) # dummy
+    return input_ids, masks, starts, ends
+    
+def read_data_eval(file, target_name, tokenizer, max_len = 384, doc_stride = 128):
+    data = json.load(open(file, encoding='utf-8'))
+    texts = [item['text'] for item in data]
+    targets = [item[target_name] for item in data]
+
+    text_tok = [tokenizer.encode(text, add_special_tokens =False) for text in texts]
+        
+    all_input_ids, all_masks, all_starts, all_ends  = [], [], [], []
+    sample_loc = [0]
+    for i in tqdm(range(len(text_tok))):
+        input_ids, masks, starts, ends = get_input_eval(text_tok[i], 
+                                                   max_len = max_len, doc_stride = doc_stride)
+        all_input_ids.extend(input_ids)
+        all_masks.extend(masks)
+        all_starts.extend(starts)
+        all_ends.extend(ends)
+        sample_loc.append(sample_loc[-1] + len(input_ids))
+    
+    return all_input_ids, all_masks, all_starts, all_ends, sample_loc
+
+
